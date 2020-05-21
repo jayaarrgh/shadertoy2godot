e07b133910b1263d38836b818290709e64c69c50
Jose Sanchez <jsanchezcastillejos@gmail.com>
2020-05-20 21:26:28 -0500

    Get shades takes shader is as a parameter

--- shader_source.py
+++ shader_source.py
@@ -3,20 +3,19 @@ import requests
 
 
 class GetShaderCode:
-    def __init__(self, shader_id):
-        self.shade_id = shader_id
+    def __init__(self):
         self.api_key = os.environ['API_KEY']
 
-    def _request_shade(self):
-        response = requests.get(f"https://www.shadertoy.com/api/v1/shaders/{self.shade_id}?key={self.api_key}")
+    def _request_shader(self, shader_id):
+        response = requests.get(f"https://www.shadertoy.com/api/v1/shaders/{shader_id}?key={self.api_key}")
         return response
 
-    def get_shades(self):
-        response = self._request_shade()
+    def get_shades(self, shader_id):
+        response = self._request_shader(shader_id)
         if response.status_code != 200:
             return None
-        shader_info = response.json()['Shader']['renderpass']
+        renderpasses = response.json()['Shader']['renderpass']
         code = []
-        for shade in shader_info:
-            code.append(shade['code'])
+        for renderpass in renderpasses:
+            code.append(renderpass['code'])
         return code
